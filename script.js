01745f2c20220dcdd957975796508da11b61e6a5
Adrian <adrian.auch11@gmail.com>
2021-08-06 10:09:38 -0600

    finished js and fixed html so formatting works. Local storage also works now

--- script.js
+++ script.js
@@ -14,43 +14,45 @@ var hr18 = $("#18");
 var savebtn = $(".savebtn");
 var date = $("#currentDay");
 var today = moment();
-var TimeofDay = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
-
-
 
 //setting current time and date.
 function setDate() {
   $("#currentDay").text(
     ["Today's Date Is: "] + today.format("dddd MMM Do, YYYY")
   );
+  UpdateTime();
 }
-//Update time of day:
+//Update time of day and added in graabing from loal storage into my for loop.
 function UpdateTime() {
   var currentTime = moment().format("H");
   //varibale of current time on the hour
-  for(var i = 0; i< TimeofDay.length; i++){
-   // if timeofday is > the current time then style a box a certain way.
-   if(TimeofDay > currentTime){
-       $("#" + TimeofDay[i]).attr("style", "background-color:#77dd77");
-        // else if timeof day is < currentTime then style box certain way.
-   } else if (TimeofDay < currentTime){
-       $("#" + TimeofDay[i]).attr("style", "backgorund-color: #d3d3d3" );
-       //else if timeof Day == currentTime then style back ground like this. 
-   } else (TimeofDay == currentTime){
-       $("#" + TimeofDay[i]).attr("style", "background-color: #77dd77");
-   }
-   
-};
+  for (var i = 7; i <= 18; i++) {
+    // if timeofday is > the current time then style a box a certain way.
+    if (i > currentTime) {
+      $("#" + i).addClass("future");
+      // else if timeof day is < currentTime then style box certain way.
+    } else if (i < currentTime) {
+      $("#" + i).addClass("past");
+      //else if timeof Day == currentTime then style back ground like this.
+    } else if (i == currentTime) {
+      $("#" + i).addClass("present");
+    }
+    $("#t" + i).val(localStorage.getItem("s" + i));
+  }
 }
 
 //local stroage to save user input
-function saveSched() {
-  //want a variable that equals the users in put. 
-  //will i need this for each hour? 
-  //use .val to get input. 
-  //we will need ot get item to display to user. 
-}
+
+//want a variable that equals the users in put.
+//will i need this for each hour?
+//use .val to get input.
+//we will need ot get item to display to user.
 
 //Call Functions
-setDate()
-savebtn.on("click", saveSched)
+setDate();
+
+savebtn.on("click", function () {
+  var text = $(this).siblings("textarea").val();
+  var btnid = $(this).attr("id");
+  localStorage.setItem(btnid, text);
+});
